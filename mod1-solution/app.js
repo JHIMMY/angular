5bf17b99ecf402be07aea6a676abe769476c4593
jhimmy astoraque durán <jhimmy astoraque durn>
2016-09-08 18:54:18 -0400

    Last Update

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -12,7 +12,8 @@
       $scope.message = "";
       $scope.listOfDishesString = "";
       $scope.listOfDishesArray = [];
-      
+      $scope.inputBorder = {};
+      $scope.fontColor = {};
       
       $scope.checkIfTooMuch = function(){
          var numberItems = 0;
@@ -23,15 +24,34 @@
                numberItems++;
             }
          });
+         
          if (numberItems === 0){
             $scope.message = "Please enter data first!";
+            $scope.inputBorder = {
+                   border : "1px solid red"
+           };
+            $scope.fontColor = {
+                   color : "red"
+            };
          }
          else if (numberItems <= 3){
             $scope.message = "Enjoy!";
+            $scope.inputBorder = {
+                   border: "1px solid green"
+            };
+            $scope.fontColor = {
+                   color : "green"
+            };
          }
          else{
             $scope.message = "Too much!";
+            $scope.inputBorder = {
+                   border: "1px solid green"
+            };
+            $scope.fontColor = {
+                   color : "green"
+            };
          }
       };
-   }
+   }// END of checkIfTooMuch
 })();
