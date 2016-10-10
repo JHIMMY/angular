585e4757b6d4d971e20d815bb03ebcae5e888b69
jhimmy astoraque durán <jhimmy astoraque durn>
2016-10-10 08:38:08 -0400

    Second upload

---
+++ mod2-solution/app.js
@@ -0,0 +1,54 @@
+
+(function(){
+   'use strict';
+   
+   angular.module('ShoppingListCheckOff', [])
+   .controller('ToBuyController', ToBuyController)
+   .controller('AlreadyBoughtController', AlreadyBoughtController)
+   .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+   
+   ToBuyController.$inject = ['ShoppingListCheckOffService'];
+   function ToBuyController(ShoppingListCheckOffService){
+      var buyList = this;
+      buyList.items = ShoppingListCheckOffService.getItemsToBuy();
+      buyList.buyItem = function (index){
+         ShoppingListCheckOffService.buyItem(index);
+      }
+   }
+   
+   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+   function AlreadyBoughtController(ShoppingListCheckOffService){
+      var boughtList = this;
+      boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+      
+      
+   }
+   
+   //Implementing the service function
+   function ShoppingListCheckOffService(){
+      var service = this;
+      // to Buy items
+      var itemsToBuy = ["cookies", "crakers", "candies", "lolipops"];
+      
+      // Already bought items
+      var itemsBought = [];
+      
+      service.getItemsToBuy = function (){
+         return itemsToBuy;
+      }
+      
+      service.buyItem = function (index){
+         var removed = itemsToBuy.splice(index, 1);
+         itemsBought.push(removed[0]);
+         if (itemsBought.length == 0){
+            console.log("no hay items");
+         }
+      }
+      
+      service.getBoughtItems =  function (){
+         return itemsBought;
+      }
+      
+   }
+   
+})();
